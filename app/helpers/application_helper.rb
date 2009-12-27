286eac8f77e60222e266c682ce299b96c1e83f37
jimmytang <jimmytang@gmail.com>
2009-12-27 01:04:08 -0800

    added everything until layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
-# Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  # Return a title on a per-page basis.
+  def title
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,58 @@
+
+require 'spec_helper'
+
+describe PagesController do
+  integrate_views
+
+  before(:each) do
+      
+        @base_title = "Ruby on Rails Tutorial Sample App"    
+    end
+    
+  describe "GET 'home'" do
+    it "should be successful" do
+      get 'home'
+      response.should be_success
+    end
+    it "should have the right title" do
+          get 'home'
+          response.should have_tag("title",
+                                   @base_title + " | Home")
+    end
+  end
+
+  describe "GET 'contact'" do
+    it "should be successful" do
+      get 'contact'
+      response.should be_success
+    end
+    it "should have the right title" do
+          get 'contact'
+          response.should have_tag("title",
+                                   @base_title + " | Contact")
+    end
+  end
+
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+    it "should have the right title" do
+          get 'about'
+          response.should have_tag("title",
+                                   @base_title + " | About")
+    end
+  end
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+    it "should have the right title" do
+          get 'help'
+          response.should have_tag("title",
+                                   @base_title + " | Help")
+    end
+  end
+end
