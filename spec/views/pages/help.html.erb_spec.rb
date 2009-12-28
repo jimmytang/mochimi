1d1eb38eaf6f519b3f0978b02a1c5d7cbe073dfa
jimmytang <jimmytang@gmail.com>
2009-12-28 12:23:33 -0800

    corrected rake

--- spec/views/pages/help.html.erb_spec.rb
+++ spec/views/pages/help.html.erb_spec.rb
@@ -5,8 +5,5 @@ describe "/pages/help" do
     render 'pages/help'
   end
 
-  #Delete this example and add some real ones or delete this file
-  it "should tell you where to find the file" do
-    response.should have_tag('p', %r[Find me in app/views/pages/help])
-  end
+
 end
