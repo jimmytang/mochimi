23c1b9e61f7f1cf3559e06adc65923df28fc042f
jimmytang <jimmytang@gmail.com>
2009-12-29 23:01:35 -0800

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -41,7 +41,7 @@ ActionController::Routing::Routes.draw do |map|
     map.contact '/contact', :controller => 'pages', :action => 'contact'
     map.about   '/about',   :controller => 'pages', :action => 'about'
     map.help    '/help',    :controller => 'pages', :action => 'help'
-
+    map.signup '/signup',   :controller => 'users', :action => 'new'
   end
   
 end
