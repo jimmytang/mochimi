4da64d78f74a9aab38d482bfcace781ac847715c
jimmytang <jimmytang@gmail.com>
2010-01-06 11:41:33 -0800

    css changes

---
+++ spec/integration/layout_links_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe "Layout links" do
+
+  it "should have a Home page at '/'" do
+    get '/'
+    response.should render_template('pages/home')
+  end
+
+  it "should have a Contact page at '/contact'" do
+    get '/contact'
+    response.should render_template('pages/contact')
+  end
+
+  it "should have an About page at '/about'" do
+    get '/about'
+    response.should render_template('pages/about')
+  end
+
+  it "should have a Help page at '/help'" do
+    get '/help'
+    response.should render_template('pages/help')
+  end
+  
+  it "should have a signup page at '/signup'" do
+      get '/signup'
+      response.should render_template('users/new')
+  end
+end
